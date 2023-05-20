ee5ef4f4af1ee20e83aa73ddaec5c01b84f7bb9d
wuqiaoqiao <324518031@qq.com>
2023-05-20 22:59:31 +0800

    格式化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
-const p = import.meta.env.VITE_APP_TITLE
+const p = import.meta.env.VITE_APP_TITLE;
 </script>
 
 <template>
